2a8997d73f7d072f3238b94f03d5e9080aafa526
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-11 18:08:34 +0200

    create basic parser (ch6)

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -23,7 +23,8 @@ public class Lox {
   }
 
   private static void runFile(String path) throws IOException {
-    if (hadError) System.exit(65);
+    if (hadError)
+      System.exit(65);
 
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
@@ -47,11 +48,15 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    // Stop if there was a syntax error.
+    if (hadError)
+      return;
+
+    System.out.println(new AstPrinter().print(expression));
+
   }
 
   static void error(int line, String message) {
@@ -62,6 +67,14 @@ public class Lox {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 
 }
