bffe4688df1151be3e7b1eaf054719160eeedc29
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-13 17:43:49 +0200

    ch 10.5: return statements

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -29,7 +29,8 @@ public class GenerateAst {
         "Function   : Token name, List<Token> params," + " List<Stmt> body",
             "If         : Expr condition, Stmt thenBranch," + " Stmt elseBranch",
             "Print      : Expr expression",
-        "Var        : Token name, Expr initializer",
+        "Return     : Token keyword, Expr value",
+            "Var        : Token name, Expr initializer",
         "While      : Expr condition, Stmt body"));
   }
 
