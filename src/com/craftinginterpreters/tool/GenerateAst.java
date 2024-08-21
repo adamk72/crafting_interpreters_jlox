3b044e447297be85b8031107bc6c7d462f2decff
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-20 20:32:58 -0500

    ch13 handle super errors

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -18,25 +18,25 @@ public class GenerateAst {
         "Call     : Expr callee, Token paren, List<Expr> arguments",
         "Get      : Expr object, Token name",
         "Grouping : Expr expression",
-            "Literal  : Object value",
+        "Literal  : Object value",
         "Logical  : Expr left, Token operator, Expr right",
-            "Set      : Expr object, Token name, Expr value",
+        "Set      : Expr object, Token name, Expr value",
+        "Super    : Token keyword, Token method",
         "This     : Token keyword",
-
         "Unary    : Token operator, Expr right",
         "Variable : Token name"));
 
     defineAst(outputDir, "Stmt", Arrays.asList(
         "Block      : List<Stmt> statements",
         "Class      : Token name, Expr.Variable superclass," + " List<Stmt.Function> methods",
-            "Class      : Token name, List<Stmt.Function> methods",
+        "Class      : Token name, List<Stmt.Function> methods",
         "Expression : Expr expression",
-            "Function   : Token name, List<Token> params," + " List<Stmt> body",
+        "Function   : Token name, List<Token> params," + " List<Stmt> body",
         "If         : Expr condition, Stmt thenBranch," + " Stmt elseBranch",
-            "Print      : Expr expression",
+        "Print      : Expr expression",
         "Return     : Token keyword, Expr value",
         "Var        : Token name, Expr initializer",
-            "While      : Expr condition, Stmt body"));
+        "While      : Expr condition, Stmt body"));
   }
 
   private static void defineAst(
